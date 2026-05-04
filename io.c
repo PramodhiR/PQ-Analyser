100d40b12e96d585a4bfa41d9b85c49cfa2727cb
pramodhi <24pramu@gmail.com>
2026-05-04 18:01:38 +0100

    read each line of data correcly

--- io.c
+++ io.c
@@ -1,3 +1,38 @@
-//
-// Created by my pc on 04/05/2026.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include "io.h"
+
+WaveformSample* load_csv(const char *filename, int *n) {
+
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("Error opening file\n");
+        return NULL;
+    }
+
+    // Allocate memory for 1000 rows
+    WaveformSample *samples = malloc(1000 * sizeof(WaveformSample));
+
+    char line[256];
+    fgets(line, sizeof(line), file); // skip header
+
+    int i = 0;
+
+    while (fscanf(file, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf",
+        &samples[i].timestamp,
+        &samples[i].phase_A_voltage,
+        &samples[i].phase_B_voltage,
+        &samples[i].phase_C_voltage,
+        &samples[i].line_current,
+        &samples[i].frequency,
+        &samples[i].power_factor,
+        &samples[i].thd_percent) == 8) {
+
+        i++;
+        }
+
+    *n = i;
+
+    fclose(file);
+    return samples;
+}
